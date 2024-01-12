436bf8a1df2d8821b6259a8f1d641dd827e67124
Nigel Dcruz <nigeldcruz7@gmail.com>
2024-01-13 00:08:07 +0530

    Dashboard Page Ui

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Orders from "./Orders";
 
 const DashboardPage: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard Page</h1>
-    </div>
+    <>
+      <div className="grid grid-cols-12 gap-4 h-[calc(100vh-72px)]">
+        <aside className="col-span-2 border-e">
+          <nav>
+            <ul>
+              <li>You Orders</li>
+              <li>Sell On EcoStore</li>
+            </ul>
+          </nav>
+        </aside>
+        <main className="col-span-10">
+          <Routes>
+            <Route path="/" element={<Orders />} />
+            <Route path="dashboard/test" element={<h1>Dashbtestoard</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 };
 
