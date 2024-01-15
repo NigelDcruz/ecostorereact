8c21f971551bf732f0cf7901a76487c9556234e2
Nigel Dcruz <nigeldcruz7@gmail.com>
2024-01-15 08:46:54 +0530

    Checkout and confirmation basic Ui

--- src/pages/Cart/Checkout.tsx
+++ src/pages/Cart/Checkout.tsx
@@ -1,11 +1,124 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CheckOutPage: React.FC = () => {
-    return (
-        <div>
-            <h1>CheckOutPage</h1>
+  return (
+    <div className="px-5 my-5 grid grid-cols-12 gap-5">
+      <div className="col-span-5">
+        <h2 className="font-medium text-xl mb-4">Billing Details</h2>
+        <form className="grid grid-cols-2 gap-5 border p-5">
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="tel"
+              id="phone"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              id="city"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              id="state"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="zip">Zip</label>
+            <input
+              type="text"
+              id="zip"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              id="country"
+              className="border p-2 rounded-lg w-full"
+            />
+          </div>
+        </form>
+        <h2 className="font-medium text-xl mb-4 mt-5">Payment Method</h2>
+        <form className="border p-5">
+          <label htmlFor="cashOnDelivery" className="flex items-center cursor-pointer">
+            Cash on Delivery
+            <input
+              type="checkbox"
+              id="cashOnDelivery"
+              name="paymentMethod"
+              value="cashOnDelivery"
+              className="border p-2 rounded-lg ml-3"
+            />
+          </label>
+        </form>
+      </div>
+      <div className="col-span-4 col-start-9">
+        <div className="border shadow-md p-5 mt-12">
+          <h2 className="font-medium mb-3 text-xl text-center">
+            Order Summary
+          </h2>
+          <p className="mb-1 text-lg">
+            <span className="font-medium ">Total:</span> $300
+          </p>
+          <p className="mb-1 text-lg">
+            <span className="font-medium ">Delivery:</span> $50
+          </p>
+          <p className="mb-1 text-lg">
+            <span className="font-medium ">Subtotal:</span> $250
+          </p>
+          <Link
+            to="/confirmation"
+            className="bg-yellow-500 py-3 px-5 rounded-lg text-white my-5 block text-center"
+          >
+            Place Order
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CheckOutPage;
--- src/pages/Cart/Confirmation.tsx
+++ src/pages/Cart/Confirmation.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const ConfirmationPage: React.FC = () => {
-    return (
-        <div>
-            <h1>ConfirmationPage</h1>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-green-100 p-5">
+        <h1 className="text-center font-medium text-2xl">Thank You!</h1>
+        <p className="text-lg text-center">Your order has been placed.</p>
+
+        <h2 className="font-medium text-xl mt-3 mb-1">Order Details</h2>
+        <div className="flex flex-wrap gap-5">
+          <p><span className="font-medium">Order ID:</span> 123456789</p>
+          <p><span className="font-medium">Order Date: </span> 12/12/2020</p>
+          <p><span className="font-medium">Order Total: </span> $300</p>
+          <p><span className="font-medium">Payment Method: </span> Cash on Delivery</p>
+          <p><span className="font-medium">Delivery Address:  </span>123, Lorem Ipsum, Dolor Sit, Amet</p>
+          <p><span className="font-medium">Delivery Date: </span> 12/12/2020</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ConfirmationPage;
