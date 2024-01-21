59829829b2d599338ee9ff7b78e814ca11168108
Allison Serrano <allisonserrano171@yahoo.com>
2024-01-21 10:22:39 -0500

    fixed routing middleware issue and successfully retrieved all existing users through the end point

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const db = require('./config/connection');
+const getUsersRoutes = require('./routes/userRoutes');
 
-const PORT = 3001;
+const PORT = 5173;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the home page!');
+});
+
+app.use('/users', getUsersRoutes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
