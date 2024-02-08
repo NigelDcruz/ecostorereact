a56750c94e2312ce013ecf608897c5a2bfecb1aa
camokkkk <93404887+camokkkk@users.noreply.github.com>
2024-02-08 12:42:58 -0600

    changed_route_enpoints

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderControllers");
 
-router.get('/', orderController.getOrders);
-router.get('/:orderId', orderController.getOrder);
-
-router.post('/', orderController.createOrder);
-router.put('/:orderId', orderController.updateOrder);
-router.delete('/deleteOrder', orderController.deleteOrder);
+router.get("/dashboard", orderController.getOrders);
+router.get("/:orderId", orderController.getOrder);
+router.post("/", orderController.createOrder);
+router.put("/:orderId", orderController.updateOrder);
+router.delete("/deleteOrder", orderController.deleteOrder);
 
 module.exports = router;
